e5ed9bfe52a424eeb7ee21fc7a18c45e3e4accd6
geniusatthemoment <114457112+geniusatthemoment@users.noreply.github.com>
2023-03-01 08:56:52 +0700

    Add files via upload

---
+++ 2полугодие/26.py
@@ -0,0 +1,10 @@
+with open('26.txt') as f:
+    s = [int(x) for x in f]
+    s.pop(0)
+    s.sort(reverse = True)
+    n,mini = 1,s[0]
+    for i in range(1,len(s)):
+        if s[i] + 3 <=mini:
+            mini=s[i]
+            n+=1
+    print(n,mini)
